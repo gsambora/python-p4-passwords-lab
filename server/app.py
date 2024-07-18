4cf5896c68158d51ba59e0c01ef12aa68ba519b3
Hana <grace.sambora@gmail.com>
2024-07-17 23:07:20 -0500

    Complete lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,34 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json['username']).first()
+
+        if user and user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'message': 'error with password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {'message': 'logout successful'}, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
